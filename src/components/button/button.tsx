50925d46314fbce2e29dd97ddebe7c889c6386ed
vickonrails <victorofoegbu0009@gmail.com>
2021-03-31 03:33:16 -0700

    feat: add emotion for styling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 const Button: React.FC<Button> = (props) => {
-  return <button {...props}>{props.children}</button>
+  return <StyledButton {...props}>{props.children}</StyledButton>
 }
 
 type ButtonType = 'submit' | 'button' | 'reset'
 interface Button extends React.HTMLAttributes<HTMLButtonElement> {
   type?: ButtonType
 }
+
+const StyledButton = styled.button``
 
 Button.defaultProps = {
   type: 'submit'
