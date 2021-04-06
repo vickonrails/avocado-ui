31886d22a4a82608813ae832b17560a809ab54cc
vickonrails <victorofoegbu0009@gmail.com>
2021-04-06 01:51:26 -0700

    feat: add button component theme

---
+++ src/components/theme/components/input.theme.ts
@@ -0,0 +1,42 @@
+// import { theme } from '..'
+import colors from '../colors'
+// import borders from '../borders'
+import spacing from '../spacing'
+
+const inputTheme = {
+  variants: {
+    fill: {
+      default: {
+        background: colors.gray[6],
+        textColor: colors.gray[9]
+      },
+      hover: {},
+      focus: {}
+    },
+    outline: {
+      default: colors.red[400],
+      hover: colors.red[300]
+    },
+    unstyled: {
+      default: colors.green[500],
+      hover: colors.green[400]
+    }
+  },
+  size: {
+    sm: {
+      verticalPadding: spacing['x-small'],
+      horizontalPadding: spacing.small
+    },
+    md: {
+      verticalPadding: spacing.small,
+      horizontalPadding: spacing.medium
+    },
+    lg: {
+      verticalPadding: spacing.medium,
+      horizontalPadding: spacing.large
+    }
+  }
+}
+
+export type InputTheme = typeof inputTheme
+export { inputTheme }
