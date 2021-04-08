198a3bb4d6167b2922e01489b68760eff90231a4
vickonrails <victorofoegbu0009@gmail.com>
2021-04-07 19:15:36 -0700

    docs: setup documentation site with smooth-docs

---
+++ website/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'smooth-doc',
+      options: {
+        name: 'Smooth DOC Starter',
+        description: 'Use your own description...',
+        siteUrl: 'https://example.com',
+        sections: ['Getting-Started', 'Components']
+      }
+    }
+  ]
+}
