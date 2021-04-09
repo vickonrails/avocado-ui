f6989d8f563d8d690fd0ea732ba6f6c96f5db07d
vickonrails <victorofoegbu0009@gmail.com>
2021-04-08 23:50:07 -0700

    feat: add homepage component

---
+++ website/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '@avocado-ui/react'
+
+const HomePage = () => {
+  return (
+    <header>
+      <section>
+        <h1>Avocado</h1>
+        <p>Lorem</p>
+        <Button>See Documentation</Button>
+      </section>
+      <section>Image goes here</section>
+    </header>
+  )
+}
+
+export default HomePage
