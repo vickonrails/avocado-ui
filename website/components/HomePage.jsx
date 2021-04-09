cd68ac7b18bd456cea6b29d371c663851d7d48b6
vickonrails <victorofoegbu0009@gmail.com>
2021-04-09 00:27:16 -0700

    docs: add @emotion/react for styling

--- website/components/HomePage.jsx
+++ website/components/HomePage.jsx
@@ -2,29 +2,52 @@ import React from 'react'
 import { Button } from '@avocado-ui/react'
 import { Link } from 'gatsby'
 import { ScreenContainer } from 'smooth-doc/components'
+import styled from '@emotion/styled'
 
 import PatternBackground from '../images/hero-background.jpg'
 
 const HomePage = () => {
   return (
-    <header>
+    <StyledHeader>
       <ScreenContainer>
-        <section>
-          <h1>Build Accessible, Themeable interfaces with ease.</h1>
-          <p>
-            Avocado-ui is an experimental design system for building accessible,
-            themeable and beautiful interfaces with ease.
-          </p>
-          <Link to='/docs/getting-started'>
-            <Button>Get Started</Button>
-          </Link>
-        </section>
-        <section>
-          <img src={PatternBackground} alt='Abstract pattern' />
-        </section>
+        <div className='header-container'>
+          <section>
+            <h1>Build Accessible, Themeable interfaces with ease.</h1>
+            <p>
+              Avocado-ui is an experimental design system for building
+              accessible, themeable and beautiful interfaces with ease.
+            </p>
+            <Link to='/docs/getting-started'>
+              <Button>Get Started</Button>
+            </Link>
+          </section>
+          <section>
+            <img src={PatternBackground} alt='Abstract pattern' />
+          </section>
+        </div>
       </ScreenContainer>
-    </header>
+    </StyledHeader>
   )
 }
+
+const StyledHeader = styled.header`
+  img {
+    width: 100%;
+  }
+  .header-container {
+    display: flex;
+    flex-direction: column;
+
+    > * {
+      flex: 1;
+    }
+  }
+
+  @media (min-width: 760px) {
+    .header-container {
+      flex-direction: row;
+    }
+  }
+`
 
 export default HomePage
