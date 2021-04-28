8417462aa5e0966c8f39d971b04860e0210931e6
vickonrails <victorofoegbu0009@gmail.com>
2021-04-28 09:55:52 -0700

    feat: implement global CSS style; fixes #4

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { theme, Button } from 'avocado-ui'
+import { theme, Button, CSSReset } from 'avocado-ui'
 import { ThemeProvider } from '@emotion/react'
 
 import { ImageDemo, Avatar, Text, Header } from './components'
@@ -8,6 +8,7 @@ const App = () => {
   return (
     // Pass custom theme to theme provider
     <ThemeProvider theme={theme}>
+      <CSSReset />
       <Button loading={true}>Get Started</Button>
       <Header>Header</Header>
       <Text />
