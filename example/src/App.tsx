538a6def3f03a2880ee3eadfdb2290840e5148c5
vickonrails <victorofoegbu0009@gmail.com>
2021-06-03 16:40:10 +0100

    chore: test Box component in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,6 +8,7 @@ import {
   ModalOverlay,
   ModalContent,
   ModalFooter,
+  Box,
   Flex
 } from 'avocado-ui'
 import { ThemeProvider } from '@emotion/react'
@@ -21,50 +22,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <CSSReset />
       <div className='App'>
-        <p>
-          The Modal component is built on top of the Dialog component from
-          @headlessui/react. It comes with the Modal, ModalTitle,ModalContent
-          and ModalFooter.
-        </p>
-        <p>
-          The easiest way to get started is to import Modal, ModalTitle,
-          ModalContent, ModalOverlay and ModalFooter from @avocado-ui/react.{' '}
-        </p>
-        <Button size='sm' onClick={() => setModalOpen(true)}>
-          Open Modal
-        </Button>
-        <Modal
-          open={modalOpen}
-          onClose={() => setModalOpen(false)}
-          initialFocus={initialBtnRef}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalTitle as='h5'>Modal Heading</ModalTitle>
-            <p>
-              The Modal provides the open and onClose props for controlling
-              modal states. Setting open to true shows the modal and vice versa.
-              Passing a function to the onClose prop specifies the desired
-              action on close of the modal.
-            </p>
-
-            <ModalFooter>
-              <Flex gap={5} justifyContent='flex-start'>
-                <Button size='sm'>Another Action</Button>
-
-                <Button
-                  size='sm'
-                  onClick={() => setModalOpen(false)}
-                  variant='error'
-                  ref={initialBtnRef}
-                  buttonType='ghost'
-                >
-                  Close Modal
-                </Button>
-              </Flex>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        <Box mt={3}>Something about a box that I know of</Box>
       </div>
     </ThemeProvider>
   )
