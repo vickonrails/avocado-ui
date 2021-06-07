cb333068f23e9b284a578cfe9df7721b65dbf38b
vickonrails <victorofoegbu0009@gmail.com>
2021-06-07 16:42:33 +0100

    docs: add Avatar shape story

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { User } from 'react-feather'
 import { Story, Meta } from '@storybook/react'
 
-import { Avatar, AvatarProps, Flex } from '../../components'
+import { Avatar, AvatarProps } from '../../components'
+import { AvatarGroup } from './avatar.group'
 
+const IMG_URL_1 = `https://avatars.githubusercontent.com/u/24235881?s=400&u=b1882b58273cf6d184b35b10ef1520a429b6ebc7&v=4`
+const IMG_URL_2 = `https://images.unsplash.com/photo-1601455763557-db1bea8a9a5a?w=200`
+const IMG_URL_3 = `https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?&w=200`
+const IMG_URL_4 = `https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=200`
 export default {
   title: 'Components/Avatar',
   component: Avatar
@@ -19,40 +24,45 @@ Default.args = {
 // Sizes
 export const Sizes: Story<AvatarProps> = () => {
   return (
-    <Flex gap={8}>
+    <AvatarGroup>
       <Avatar size='xs' name='xs' />
       <Avatar size='sm' name='sm' />
       <Avatar size='md' name='md' />
       <Avatar size='lg' name='lg' />
       <Avatar size='xl' name='xl' />
-    </Flex>
+    </AvatarGroup>
   )
 }
 
 export const WithImages: Story<AvatarProps> = () => {
-  const IMG_URL_1 = `https://avatars.githubusercontent.com/u/24235881?s=400&u=b1882b58273cf6d184b35b10ef1520a429b6ebc7&v=4`
-  const IMG_URL_2 = `https://images.unsplash.com/photo-1601455763557-db1bea8a9a5a?w=200`
-  const IMG_URL_3 = `https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?&w=200`
-  const IMG_URL_4 = `https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=200`
   return (
-    <Flex>
+    <AvatarGroup>
       <Avatar size='lg' src={IMG_URL_1} />
       <Avatar size='lg' src={IMG_URL_2} />
       <Avatar size='lg' src={IMG_URL_3} />
       <Avatar size='lg' src={IMG_URL_4} />
-    </Flex>
+    </AvatarGroup>
   )
 }
 
 export const BackgroundColors: Story<AvatarProps> = () => {
   return (
-    <Flex gap={10}>
+    <AvatarGroup>
       <Avatar size='lg' name='Kind Major' bgColor='blue' />
       <Avatar size='lg' name='Kind Major' bgColor='red' />
       <Avatar size='lg' name='Kind Major' bgColor='black' />
       <Avatar size='lg' name='Kind Major' bgColor='magenta' />
-    </Flex>
+    </AvatarGroup>
   )
 }
-// With SRC
-// Background color
+
+export const Shapes: Story<AvatarProps> = () => {
+  const IMG_SRC = `https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=200`
+  return (
+    <AvatarGroup>
+      <Avatar shape='curve' size='lg' name='Kind Major' src={IMG_SRC} />
+      <Avatar shape='round' size='lg' name='Kind Major' src={IMG_SRC} />
+      <Avatar shape='square' size='lg' name='Kind Major' src={IMG_SRC} />
+    </AvatarGroup>
+  )
+}
