0a727c50117dfc0014e8b062f80a2183d11f2ad1
vickonrails <victorofoegbu0009@gmail.com>
2021-06-22 15:56:24 +0100

    chore: test Card component locally

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { ThemeProvider, CSSReset, Box } from 'avocado-ui'
+import { ThemeProvider, CSSReset, Card } from 'avocado-ui'
 
 import './index.css'
-import { Header } from './components'
 
 const App = () => {
   return (
     <ThemeProvider theme={{}}>
       <CSSReset />
       <div className='App'>
-        <Header />
-        <Box mt={3}>Something about a box that I know of</Box>
+        <Card p={8} br={4} showShadow>
+          Something about a box that I know of
+        </Card>
       </div>
     </ThemeProvider>
   )
